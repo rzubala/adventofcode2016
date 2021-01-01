32afdcf52192d8fbcb39df1d7f825608337e3d3d
Rafał Zubala <rzubala@gmail.com>
2021-01-01 15:29:00 +0100

    day7 part2

--- js/07/solve.ts
+++ js/07/solve.ts
@@ -2,6 +2,7 @@ import { FileReader } from "../common";
 
 class Solve7 extends FileReader {
   private data: string[] = [];
+  private parsed: Array<Array<Array<string>>> = []
 
   private init = async () => {
     try {
@@ -15,24 +16,42 @@ class Solve7 extends FileReader {
 
   run = async () => {
     await this.init();
-    this.process();
+    this.process1();
+    this.process2();
   };
 
-  private process = () => {
+  private process2 = () => {
+    let cnt = 0
+    for (let line of this.parsed) {
+      for (let hyper of line[0]) {
+        if (hasAba(hyper, line[1])) {
+          cnt++
+        }
+      }
+    }
+    console.log(cnt)
+  }
+
+  private process1 = () => {
     let cnt = 0
     for (let ip of this.data) {
+      const parsedIp = []
       let index = 0
       let hyper = undefined
       let len = ip.length
       let abba1 = false
       let abba2 = false
+      const parsedNormal = []
+      const parsedHyper = []
       while (index < len) {
         if (hyper) {
           index = ip.indexOf(']', hyper)
           if (index === -1) {
-            throw new Error('something went wron')
+            throw new Error('something went wrong')
           }
-          abba1 = abba1 || hasAbba(ip.slice(hyper, index))
+          const part = ip.slice(hyper, index)
+          abba1 = abba1 || hasAbba(part)
+          parsedHyper.push(part)
           hyper = undefined
           index++
         } else {
@@ -43,15 +62,43 @@ class Solve7 extends FileReader {
           } else {
             hyper = index + 1
           }
-          abba2 = abba2 || hasAbba(ip.slice(from, index))
+          const part = ip.slice(from, index)
+          parsedNormal.push(part)
+          abba2 = abba2 || hasAbba(part)
         }        
       }
       if (!abba1 && abba2) {
         cnt++
       }
+      parsedIp.push(parsedHyper)
+      parsedIp.push(parsedNormal)
+      this.parsed.push(parsedIp)
     }
     console.log(cnt)
   };
+}
+
+const hasAba = (hyper: string, parts: string[]): boolean => {
+  const len = hyper.length
+  let pos = 0
+  while (pos + 2 < len) {
+    if (hyper[pos] === hyper[pos+2] && hyper[pos] !== hyper[pos+1]) {
+      if (checkBab(hyper.slice(pos, pos+2), parts)) {
+        return true
+      }
+    }
+    pos++
+  }
+}
+
+const checkBab = (input: string, parts: string[]): boolean => {
+  const bab = input[1] + input[0] + input[1]
+  for (let part of parts) {
+    if (part.includes(bab)) {
+      return true
+    }
+  }
+  return false
 }
 
 const hasAbba = (input: string): boolean => {
