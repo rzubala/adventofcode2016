5d57a489a6e1cf528bdc63b93aed2d4baee30601
Rafał Zubala <rzubala@gmail.com>
2021-01-06 15:20:53 +0100

    day9

--- js/09/solve.ts
+++ js/09/solve.ts
@@ -1,5 +1,9 @@
 import { FileReader } from "../common";
 
+const getLength = (input: string): number => {
+  return input.replace(' ', '').length
+}
+
 class Solve9 extends FileReader {
   private data: string[] = [];
 
@@ -15,10 +19,42 @@ class Solve9 extends FileReader {
 
   run = async () => {
     await this.init();
-    this.process();
+    this.process1();
+    this.process2();
   };
 
-  private process = () => {
+  private process2 = () => {
+    let result = 0    
+    for (let test of this.data) {
+      result += this.decompress(test, 1)
+    }
+    console.log(result)
+  }
+
+  private decompress = (sequence: string, repeat: number): number => {
+    let index = 0;
+    let result = 0;
+    while (true) {
+      let nextIndex = sequence.indexOf("(", index);
+      if (nextIndex === -1) {
+        break;
+      }
+      if (index !== nextIndex) {
+        result += getLength(sequence.slice(index, nextIndex)) * repeat;
+      }
+      nextIndex++;
+      const to = sequence.indexOf(")", nextIndex);
+      const cmd = sequence.slice(nextIndex, to).split("x");
+      result += this.decompress(sequence.slice(to + 1, to + 1 + +cmd[0]), +cmd[1]) * repeat     
+      index = to + 1 + +cmd[0];
+    }
+    if (index < sequence.length) {
+      result += getLength(sequence.slice(index)) * repeat;
+    }
+    return result
+  }
+
+  private process1 = () => {
     for (let test of this.data) {
       let index = 0;
       let result = "";
